13f7067cf82f158d61750ee355f6446d0bccf297
Paul Soucy <paul.soucy@videray.com>
2019-01-31 15:40:06 -0500

    wip

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <div class="card-text">
             <?php the_content(); ?>
         </div>
+        <p class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>
     </div>
 </div>
 
